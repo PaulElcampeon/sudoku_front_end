cc2784c5fcdb5500f2a534f1ede1ef9c6a616007
PaulElcampeon <pauloladele@live.co.uk>
2020-07-13 18:54:13 +0100

    Initial setup.

---
+++ src/components/SudokuBoard.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import SudokuValueBox from './SudokuValueBox';
+
+
+const SudokuBoard = () => {
+
+    return (
+        <div className="sudoku_board">
+
+        </div>
+    )
+}
+
+export default SudokuBoard;
---
+++ src/components/SudokuValueBox.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const SudokuValueBox = () => {
+
+    return (
+        <div className="sudoku_valeu_box">
+
+        </div>
+    )
+}
+
+export default SudokuValueBox;
