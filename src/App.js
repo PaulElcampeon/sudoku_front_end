38da836898992391127a9f1a07fea87dddc5280c
PaulElcampeon <pauloladele@live.co.uk>
2020-07-13 20:37:48 +0100

    Added SudokuBoard component.

--- src/App.js
+++ src/App.js
@@ -1,10 +1,13 @@
 import React from 'react';
+import SudokuBoard from './components/SudokuBoard';
 import './styles/App.css';
+import './styles/SudokuBoard.css';
+import './styles/SudokuValueBox.css';
 
 function App() {
   return (
     <div className="App">
-      
+      <SudokuBoard />
     </div>
   );
 }
