4d7f6d884631c510263ea27f2fe5ca4c1f0541e6
PaulElcampeon <pauloladele@live.co.uk>
2020-07-14 10:00:44 +0100

    Added input tags to allow user to input values in empty or zero value cells.

--- src/components/SudokuValueBox.js
+++ src/components/SudokuValueBox.js
@@ -1,11 +1,26 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 const SudokuValueBox = (props) => {
+    const [value, setValue] = useState(props.value);
 
-    const styleName = props.value === 0? "sudoku_value_box unsolved" : "sudoku_value_box";
+    const styleName = props.value === 0 ? "sudoku_value_box unsolved" : "sudoku_value_box";
+
+    const UpdateValue = (e) => {
+        if (props.value === 0) {
+            if (e.target.value === "") {
+                setValue(0)
+            } else {
+                const tempValue = e.target.value;
+                const tempArray = tempValue.split("");
+                setValue(tempArray.pop())
+            }
+        }
+    }
+
     return (
         <div className={styleName}>
-            {props.value}
+            {/* {props.value} */}
+            <input className="value_box_input" type="text" value={props.value === 0 ? value : props.value} onChange={UpdateValue} />
         </div>
     )
 }
