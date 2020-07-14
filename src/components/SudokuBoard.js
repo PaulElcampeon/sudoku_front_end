56fdd28178e4622ac6c26fd5692f1a8d2ac2222d
PaulElcampeon <pauloladele@live.co.uk>
2020-07-14 09:58:47 +0100

    Refactoring and implemented GetSolution function.

--- src/components/SudokuBoard.js
+++ src/components/SudokuBoard.js
@@ -3,25 +3,20 @@ import SudokuValueBox from './SudokuValueBox';
 
 const SudokuBoard = () => {
     const [cells, setCells] = useState(new Array(81).fill(0));
+    const [originalBoard, setOriginalBoard] = useState([]);
 
     useEffect(() => {
         GetBoard();
     }, []);
 
     const GetBoard = () => {
-          fetch('https://sugoku.herokuapp.com/board?difficulty=easy', {
+        fetch('https://sugoku.herokuapp.com/board?difficulty=easy', {
             method: 'get'
         })
             .then(response => response.json())
             .then(data => {
-                const board = data.board;
-                var tempValuesHolder = [];
-                for (let i = 0; i < board.length; i++) {
-                    for (let y = 0; y < board.length; y++) {
-                        tempValuesHolder.push(board[i][y]);
-                    }
-                }
-                setCells(tempValuesHolder);
+                setOriginalBoard(data.board);
+                PopulateBoard(data.board);
             })
             .catch((err) => {
                 // Error :(
@@ -29,8 +24,33 @@ const SudokuBoard = () => {
     }
 
     const GetSolution = () => {
+        fetch('/api/sudoku/solution', {
+            method: 'post',
+            body: JSON.stringify(originalBoard)
+        })
+            .then(response => response.json())
+            .then(data => {
+                if (data.isSuccessful) {
+                    PopulateBoard(data.solution);
+                } else {
 
-    } 
+                }
+            })
+            .catch((err) => {
+                // Error :(
+            });
+    }
+
+    const PopulateBoard = (board) => {
+        var tempValuesHolder = [];
+
+        for (let i = 0; i < board.length; i++) {
+            for (let y = 0; y < board.length; y++) {
+                tempValuesHolder.push(board[i][y]);
+            }
+        }
+        setCells(tempValuesHolder);
+    }
 
     return (
         <div className="sudoku_board">
